c3e9ae1960b911d7c13f00f68c708ce7387d7d65
Dahiorus <brondon.ung@gmail.com>
2017-02-27 21:42:11 +0100

    Tests unitaires sur AuthenticationController
    Ajout des tests couvrant les cas suivants :
      * erreur 403
      * erreur 404
    
    Ajout d'un logger

--- src/ColocMatching/CoreBundle/Tests/Controller/Rest/v1/AuthenticationControlerTest.php
+++ src/ColocMatching/CoreBundle/Tests/Controller/Rest/v1/AuthenticationControlerTest.php
@@ -3,46 +3,59 @@
 namespace ColocMatching\CoreBundle\Tests\Controller\Rest\v1;
 
 use ColocMatching\CoreBundle\Entity\User\User;
+use ColocMatching\CoreBundle\Exception\UserNotFoundException;
 use ColocMatching\CoreBundle\Manager\User\UserManager;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\BrowserKit\Client;
 
 class AuthenticationControlerTest extends WebTestCase {
 
     /**
      * @var Client
      */
-    private static $client;
+    private $client;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     private $userManager;
 
 
-    public static function setUpBeforeClass() {
-        self::$client = parent::createClient();
-        self::$client->setServerParameter("HTTP_HOST", "coloc-matching.api");
-    }
-
-
     protected function setUp() {
-        $this->userManager = self::createMock(UserManager::class);
-        self::$client->getKernel()->getContainer()->set("coloc_matching.core.user_manager", $this->userManager);
+        $this->client = parent::createClient();
+        $this->client->setServerParameter("HTTP_HOST", "coloc-matching.api");
+
+        $this->userManager = parent::createMock(UserManager::class);
+        $this->client->getKernel()->getContainer()->set("coloc_matching.core.user_manager", $this->userManager);
+
+        $this->logger = $this->client->getKernel()->getContainer()->get("logger");
     }
 
 
-    public function testPostAuthTokenAction() {
-        $this->mockFindByUsername();
+    public function testPostAuthTokenActionWith201() {
+        $this->logger->info("Test authenticating a user with success");
 
         $username = "user@test.fr";
-        self::$client->request("POST", "/rest/auth-tokens/",
+        $user = $this->createUser($username, true);
+        $this->userManager->expects($this->once())->method("findByUsername")->with($username)->willReturn($user);
+
+        $this->client->request("POST", "/rest/auth-tokens/",
             array ("_username" => $username, "_password" => "password"), [ ], [ "CONTENT_TYPE" => "application/json"]);
 
         /** @var Response */
-        $response = self::$client->getResponse();
+        $response = $this->client->getResponse();
         $data = json_decode($response->getContent(), true);
 
         $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode(),
             sprintf("Expected response status code to be equal to 201, but got %d", $response->getStatusCode()));
+
         $this->assertNotEmpty($data["token"], "Expected 'token' field to be not empty");
         $this->assertNotEmpty($data["user"], "Expected 'user' field to be not empty");
         $this->assertEquals($username, $data["user"]["username"],
@@ -50,16 +63,70 @@ class AuthenticationControlerTest extends WebTestCase {
     }
 
 
-    private function mockFindByUsername() {
+    public function testPostAuthTokenActionWith403() {
+        $this->logger->info("Test authenticating a user with forbidden access");
+
+        $username = "user@test.fr";
+        $user = $this->createUser($username, false);
+        $this->userManager->expects($this->once())->method("findByUsername")->with($username)->willReturn($user);
+
+        $this->client->request("POST", "/rest/auth-tokens/",
+            array ("_username" => $username, "_password" => "password"), [ ], [ "CONTENT_TYPE" => "application/json"]);
+
+        /** @var Response */
+        $response = $this->client->getResponse();
+
+        $this->assertEquals(Response::HTTP_FORBIDDEN, $response->getStatusCode(),
+            sprintf("Expected response status code to be equal to 401, but got %d", $response->getStatusCode()));
+    }
+
+
+    public function testPostAuthTokenActionOnNonExistingUser() {
+        $this->logger->info("Test authenticating a non existing user");
+
+        $username = "user@test.fr";
+        $this->userManager->expects($this->once())->method("findByUsername")->with($username)->willThrowException(
+            new UserNotFoundException("username", $username));
+
+        $this->client->request("POST", "/rest/auth-tokens/",
+            array ("_username" => $username, "_password" => "password"), [ ], [ "CONTENT_TYPE" => "application/json"]);
+
+        /** @var Response */
+        $response = $this->client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode(),
+            sprintf("Expected response status code to be equal to 404, but got %d", $response->getStatusCode()));
+    }
+
+
+    public function testPostAuthTokenActionWithBadCredentials() {
+        $this->logger->info("Test authenticating a user with bad credentials");
+
+        $username = "user@test.fr";
+        $user = $this->createUser($username, true);
+        $this->userManager->expects($this->once())->method("findByUsername")->with($username)->willReturn($user);
+
+        $this->client->request("POST", "/rest/auth-tokens/", array ("_username" => $username, "_password" => "toto"),
+            [ ], [ "CONTENT_TYPE" => "application/json"]);
+
+        /** @var Response */
+        $response = $this->client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode(),
+            sprintf("Expected response status code to be equal to 404, but got %d", $response->getStatusCode()));
+    }
+
+
+    private function createUser(string $username, bool $enabled): User {
         $user = new User();
 
-        $user->setEmail("user@test.fr");
+        $user->setEmail($username);
         $user->setFirstname("User");
         $user->setLastname("Test");
         $user->setPassword(password_hash("password", PASSWORD_BCRYPT, [ "cost" => 12]));
-        $user->setEnabled(true);
+        $user->setEnabled($enabled);
 
-        $this->userManager->expects($this->any())->method("findByUsername")->with("user@test.fr")->willReturn($user);
+        return $user;
     }
 
 }
