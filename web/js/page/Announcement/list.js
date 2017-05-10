41e607d0310d63d334b845b7e26ee98461d12c31
Dahiorus <brondon.ung@gmail.com>
2017-05-11 00:38:48 +0200

    Ajout du formulaire de recherche d'annonces

--- web/js/page/Announcement/list.js
+++ web/js/page/Announcement/list.js
@@ -5,6 +5,7 @@
 $(document).ready(function (e) {
 	$('ul.sidebar-menu > li#announcements').addClass('active');
 	
+	// loading announcements
 	$.get('/admin/announcement/list', {
 		page: 1,
 		limit: 20,
@@ -14,6 +15,24 @@ $(document).ready(function (e) {
 		$('div#list-content').html(data);
 	});
 	
+	// setting rent price range slider
+	$('input#rent-price-range').ionRangeSlider({
+		type: 'double',
+		grid: true,
+		min: 300,
+		max: 1500,
+		step: 50,
+		postfix: ' €',
+		prettify_enabled: true
+	});
+	
+	// setting datepicker locale
+	$('.date').datepicker({
+		language: conf.locale
+	});
+	
+	// setting Google address autocomplete
+	initAddressAutocomplete('input#address');
 }).ajaxComplete(function () {
 	onChangePage();
 	onChangeSize();
@@ -27,11 +46,16 @@ function onChangePage() {
 		if (!$(this).closest('li').hasClass('disabled')) {
 			var /*string*/ url = $(this).attr('href');
 			
-			$.get(url, {}, function (data, status, jqXHR) {
-				// get filters
-				
-				$('div#list-content').html(data);
-			});
+			if (url.includes('list')) {
+				$.get(url, {}, function (data, status, jqXHR) {
+					$('div#list-content').html(data);
+				});
+			}
+			else if (url.includes('search')) {
+				// get search filters
+			}
+			
+			
 		}
 	});
 }
@@ -43,10 +67,28 @@ function onChangeSize() {
 		
 		var /*string*/ url = $(this).find('a').attr('href');
 		
-		$.get(url, {}, function (data, status, jqXHR) {
-			// get filters
-			
-			$('div#list-content').html(data);
-		});
+		if (url.includes('list')) {
+			$.get(url, {}, function (data, status, jqXHR) {
+				$('div#list-content').html(data);
+			});
+		}
+		else if (url.includes('search')) {
+			// get search filters
+		}
 	});
+}
+
+
+function initAddressAutocomplete(/*string*/ inputSelector) {
+	var /*jQuery*/ $input = $(inputSelector);
+	var /*Autocomplete*/ autocomplete = new google.maps.places.Autocomplete($input.get(0), {
+		componentRestrictions: {
+			country: conf.locale
+		}
+	});
+}
+
+
+function getSearchFilter() {
+	
 }
