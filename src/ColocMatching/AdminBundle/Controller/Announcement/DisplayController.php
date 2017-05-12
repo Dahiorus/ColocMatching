7039d6dd6ae65f78c3f8846ed6d00bea972c0018
Dahiorus <brondon.ung@gmail.com>
2017-05-13 00:32:21 +0200

    Début de développement de la page d'une annonce

---
+++ src/ColocMatching/AdminBundle/Controller/Announcement/DisplayController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ColocMatching\AdminBundle\Controller\Announcement;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use ColocMatching\CoreBundle\Entity\Announcement\Announcement;
+
+/**
+ * @Route(path="/announcement")
+ *
+ * @author Dahiorus
+ */
+class DisplayController extends Controller {
+
+
+    /**
+     * @Route(methods={"GET"}, path="/{id}", name="admin_announcement", requirements={"id"="\d+"})
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getPageAction($id) {
+        $this->get("logger")->info(sprintf("Getting announcement display page [id: %d]", $id));
+
+        /** @var Announcement */
+        $announcement = $this->get("coloc_matching.core.announcement_manager")->read($id);
+
+        return $this->render("AdminBundle:Announcement:display.html.twig", array ("announcement" => $announcement));
+    }
+
+}
---
+++ web/js/page/Announcement/display.js
@@ -0,0 +1,10 @@
+/**
+ * Script of the view /admin/announcement/{id}
+ */
+
+$(document).ready(function (e) {
+	$('ul.sidebar-menu > #announcements').addClass('active');
+
+}).ajaxComplete(function () {
+	
+});
