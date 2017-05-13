b58e0379e6ce9457feaa701d442b8ff4a86a42be
Dahiorus <brondon.ung@gmail.com>
2017-05-13 20:34:58 +0200

    Création d'un script JS global à toutes les pages

--- web/static/js/page/Announcement/list.js
+++ web/static/js/page/Announcement/list.js
@@ -26,11 +26,6 @@ $(document).ready(function (e) {
 		prettify_enabled: true
 	});
 	
-	// setting datepicker locale
-	$('.date').datepicker({
-		language: conf.locale
-	});
-	
 	// setting Google address autocomplete
 	initAddressAutocomplete('input#address');
 	
@@ -127,16 +122,6 @@ function onSubmitSearch() {
 }
 
 
-function initAddressAutocomplete(/*string*/ inputSelector) {
-	var /*jQuery*/ $input = $(inputSelector);
-	var /*Autocomplete*/ autocomplete = new google.maps.places.Autocomplete($input.get(0), {
-		componentRestrictions: {
-			country: conf.locale
-		}
-	});
-}
-
-
 function getSearchFilter() {
 	var /*jQuery*/ $form = $('#search-form');
 	var /*Object*/ filter = {};
@@ -158,19 +143,4 @@ function getSearchFilter() {
 	filter.order = $selectedSort.data('order');
 	
 	return filter;
-}
-
-
-function extractUrlParams(/*string*/ url) {
-	var /*string*/ query = url.split('?')[1];
-	var /*array*/ joinedParams = query.split('&');
-	var /*array*/ params = {};
-	
-	for (var i = 0; i < joinedParams.length; i++) {
-		var words = joinedParams[i].split('=');
-		
-		params[words[0]] = words[1];
-	}
-	
-	return params;
 }
--- web/static/js/page/global.js
+++ web/static/js/page/global.js
@@ -2,6 +2,37 @@
  * Global application JS script
  */
 $(document).ready(function (e) {
+	// adding skin to the page
 	$('body').addClass('sidebar-mini skin-blue fixed');
 	$('.breadcrumb').find('.breadcrumb-item').last().addClass('active');
+	
+	// setting datepicker locale
+	$('.date').datepicker({
+		language: conf.locale
+	});
 });
+
+
+function initAddressAutocomplete(/*string*/ inputSelector) {
+	var /*jQuery*/ $input = $(inputSelector);
+	var /*Autocomplete*/ autocomplete = new google.maps.places.Autocomplete($input.get(0), {
+		componentRestrictions: {
+			country: conf.locale
+		}
+	});
+}
+
+
+function extractUrlParams(/*string*/ url) {
+	var /*string*/ query = url.split('?')[1];
+	var /*array*/ joinedParams = query.split('&');
+	var /*array*/ params = {};
+	
+	for (var i = 0; i < joinedParams.length; i++) {
+		var words = joinedParams[i].split('=');
+		
+		params[words[0]] = words[1];
+	}
+	
+	return params;
+}
