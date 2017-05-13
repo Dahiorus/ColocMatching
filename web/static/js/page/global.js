f3aa30df152f335727082f606ff378ad8ec40ac9
Dahiorus <brondon.ung@gmail.com>
2017-05-13 20:27:14 +0200

    Déplacement des fichiers CSS et JS dans un répertoire static/

---
+++ web/static/js/page/global.js
@@ -0,0 +1,7 @@
+/**
+ * Global application JS script
+ */
+$(document).ready(function (e) {
+	$('body').addClass('sidebar-mini skin-blue fixed');
+	$('.breadcrumb').find('.breadcrumb-item').last().addClass('active');
+});
