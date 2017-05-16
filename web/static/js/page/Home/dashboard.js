60b6cc8911c55b5aac3f47af1b452c97c38c6979
Dahiorus <brondon.ung@gmail.com>
2017-05-17 00:53:39 +0200

    Ajout de Google Maps sur le dashboard et suppression du graphique
    La Google Map permet de localiser les dernières annonces postées.

--- web/static/js/page/Home/dashboard.js
+++ web/static/js/page/Home/dashboard.js
@@ -2,36 +2,31 @@
  * Script of the view /admin
  */
 
-$(document).ready(function () {
-//	var /*RenderingContext*/ context = $('#user-chart').get(0).getContext('2d'); 
-//	var /*Chart*/ userChart = new Chart(context, {
-//		type: 'bar',
-//		data: {
-//			labels: ['lab1', 'lab2', 'lab3'],
-//			datasets: [{
-//				label: 'Test',
-//				data: [40, 23, 57],
-//				backgroundColor: [
-//					'rgba(255, 99, 132, 0.2)',
-//	                'rgba(54, 162, 235, 0.2)',
-//	                'rgba(255, 206, 86, 0.2)'
-//				],
-//				borderColor: [
-//					'rgba(255,99,132,1)',
-//	                'rgba(54, 162, 235, 1)',
-//	                'rgba(255, 206, 86, 1)'
-//				],
-//				borderWidth: 1
-//			}]
-//		},
-//		options: {
-//	        scales: {
-//	            yAxes: [{
-//	                ticks: {
-//	                    beginAtZero:true
-//	                }
-//	            }]
-//	        }
-//	    }
-//	});
+$(document).ready(function() {
+	var total = $('#announcement-map').data('total');
+	
+	initMap();
 });
+
+
+function initMap() {
+	// center of France
+	var /*LatLng*/ center = new google.maps.LatLng(46.52863469527167, 2.43896484375);
+	var /*jQuery*/ $mapContainer = $('#announcement-map');
+	
+	var /*Map*/ map = new google.maps.Map($mapContainer.get(0), {
+		zoom: 6,
+		center: center
+	});
+	
+	$('span.announcement-latlng').each(function (index, object) {
+		var lat = $(object).data('lat');
+		var lng = $(object).data('lng');
+		
+		var position = new google.maps.LatLng(lat, lng);
+		var marker = new google.maps.Marker({
+			position: position,
+			map: map
+		});
+	});
+}
