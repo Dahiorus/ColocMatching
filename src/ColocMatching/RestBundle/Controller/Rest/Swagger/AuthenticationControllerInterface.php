7bf4b3bf4d92875eb97e702f56d548a1f3a2504d
Dahiorus <brondon.ung@gmail.com>
2017-11-28 00:14:11 +0100

    Correction mineure

--- src/ColocMatching/RestBundle/Controller/Rest/Swagger/AuthenticationControllerInterface.php
+++ src/ColocMatching/RestBundle/Controller/Rest/Swagger/AuthenticationControllerInterface.php
@@ -17,7 +17,6 @@ interface AuthenticationControllerInterface {
      * Authenticates a user and creates an authentication token
      *
      * @SWG\Post(path="/auth-tokens/", operationId="rest_create_authtoken", tags={ "Authentication" },
-     *
      *   @SWG\Parameter(
      *     in="body", name="credentials", required=true, description="The User credentials",
      *     @SWG\Schema(
