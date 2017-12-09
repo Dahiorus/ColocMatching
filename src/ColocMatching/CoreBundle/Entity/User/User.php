3275935d247d80075047dc0a918728d4ef02da70
Dahiorus <brondon.ung@gmail.com>
2017-12-09 13:12:41 +0100

    Correction mineure

--- src/ColocMatching/CoreBundle/Entity/User/User.php
+++ src/ColocMatching/CoreBundle/Entity/User/User.php
@@ -118,10 +118,8 @@ class User implements UserInterface, Updatable, Visitable {
     /**
      * @var string
      *
-     * @Assert\NotBlank(groups={ "Create", "UpdatePassword" })
+     * @Assert\NotBlank(groups={ "Create" })
      * @Assert\Length(min=8, max=4096)
-     * @SWG\Property(
-     *   description="User password (used only in POST, PUT, PATCH operations)", minLength=8, maxLength=4096)
      */
     private $plainPassword;
 
