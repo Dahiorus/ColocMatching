92119e3ddf72b03b851e754f8ddde5cc87baa54e
Dahiorus <brondon.ung@gmail.com>
2018-03-15 23:50:18 +0100

    Les entités sont triées par ordre de date de création ASC par défaut

--- src/ColocMatching/CoreBundle/Repository/Filter/PageableFilter.php
+++ src/ColocMatching/CoreBundle/Repository/Filter/PageableFilter.php
@@ -9,7 +9,8 @@ use Swagger\Annotations as SWG;
  *
  * @author Dahiorus
  */
-class PageableFilter {
+class PageableFilter
+{
 
     const ORDER_ASC = "asc";
 
@@ -47,66 +48,76 @@ class PageableFilter {
      *
      * @var string
      *
-     * @SWG\Property(description="Attribute name to sort by", default="id")
+     * @SWG\Property(description="Attribute name to sort by", default="createdAt")
      */
-    protected $sort = "id";
+    protected $sort = "createdAt";
 
 
-    public function __toString() : string {
+    public function __toString() : string
+    {
         return sprintf("PageableFilter [page=%d, size=%d, order='%s', sort='%s']", $this->page, $this->size,
             $this->order, $this->sort);
     }
 
 
-    public function getPage() : int {
+    public function getPage() : int
+    {
         return $this->page;
     }
 
 
-    public function setPage($page) {
+    public function setPage($page)
+    {
         $this->page = $page;
 
         return $this;
     }
 
 
-    public function getSize() : int {
+    public function getSize() : int
+    {
         return $this->size;
     }
 
 
-    public function setSize($size) {
+    public function setSize($size)
+    {
         $this->size = $size;
 
         return $this;
     }
 
 
-    public function getOrder() : string {
+    public function getOrder() : string
+    {
         return $this->order;
     }
 
 
-    public function setOrder($order) {
+    public function setOrder($order)
+    {
         $this->order = strtolower($order);
 
         return $this;
     }
 
 
-    public function getSort() : string {
+    public function getSort() : string
+    {
         return $this->sort;
     }
 
 
-    public function setSort($sort) {
+    public function setSort($sort)
+    {
         $this->sort = $sort;
 
         return $this;
     }
 
 
-    public function getOffset() : int {
+    public function getOffset() : int
+    {
         return ($this->page - 1) * $this->size;
     }
 
