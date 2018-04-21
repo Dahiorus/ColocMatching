f5f4f6932e331b17600c158e8554ba016af6a803
Dahiorus <brondon.ung@gmail.com>
2018-04-21 19:42:21 +0200

    Suppression d'annotation Swagger et JMS Serializer

--- src/ColocMatching/CoreBundle/Entity/Invitation/AnnouncementInvitation.php
+++ src/ColocMatching/CoreBundle/Entity/Invitation/AnnouncementInvitation.php
@@ -4,9 +4,6 @@ namespace ColocMatching\CoreBundle\Entity\Invitation;
 
 use ColocMatching\CoreBundle\Entity\Announcement\Announcement;
 use Doctrine\ORM\Mapping as ORM;
-use Hateoas\Configuration\Annotation as Hateoas;
-use JMS\Serializer\Annotation as JMS;
-use Swagger\Annotations as SWG;
 
 /**
  * Class AnnouncementInvitation
@@ -16,18 +13,6 @@ use Swagger\Annotations as SWG;
  *   uniqueConstraints={
  *     @ORM\UniqueConstraint(name="UK_ANNOUNCE_INV_RECIPIENT", columns={ "recipient_id", "announcement_id" }),
  * })
- * @JMS\ExclusionPolicy("ALL")
- * @SWG\Definition(definition="AnnouncementInvitation", allOf={ @SWG\Schema(ref="#/definitions/Invitation") })
- * @Hateoas\Relation(
- *   name="self",
- *   href= @Hateoas\Route(name="rest_get_announcement_invitation", absolute=true,
- *     parameters={ "id" = "expr(object.getInvitable().getId())", "invitationId" = "expr(object.getId())" })
- * )
- * @Hateoas\Relation(
- *   name= "invitable",
- *   href= @Hateoas\Route(name="rest_get_announcement", absolute=true,
- *     parameters={ "id" = "expr(object.getInvitable().getId())" })
- * )
  */
 class AnnouncementInvitation extends Invitation
 {
