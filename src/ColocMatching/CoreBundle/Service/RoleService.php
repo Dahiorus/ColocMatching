052ddb2a2686d217a850bd9ded8de94d61ed3edd
Dahiorus <brondon.ung@gmail.com>
2018-05-21 13:11:03 +0200

    Création d'un service testant le rôle d'un utilisateur

---
+++ src/ColocMatching/CoreBundle/Service/RoleService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ColocMatching\CoreBundle\Service;
+
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Service to test user's roles
+ *
+ * @author Dahiorus
+ */
+class RoleService
+{
+    /** @var RoleHierarchyInterface */
+    private $roleHierarchy;
+
+
+    public function __construct(RoleHierarchyInterface $roleHierarchy)
+    {
+        $this->roleHierarchy = $roleHierarchy;
+    }
+
+
+    /**
+     * Tests if the specified user has a specific role
+     *
+     * @param string $role The role value
+     * @param UserInterface $user The user
+     *
+     * @return bool
+     */
+    public function isGranted(string $role, UserInterface $user) : bool
+    {
+        $r = new Role($role);
+
+        foreach ($user->getRoles() as $userRole)
+        {
+            /** @var Role[] $userRoles */
+            $userRoles = $this->roleHierarchy->getReachableRoles(array (new Role($userRole)));
+
+            if (in_array($r, $userRoles))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
