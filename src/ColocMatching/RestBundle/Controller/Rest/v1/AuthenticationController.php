10d4581d1bc2556d12500c8c652667a432227e54
Dahiorus <brondon.ung@gmail.com>
2018-06-12 00:41:38 +0200

    Web service d'authentification d'utilisateur d'applications externes

--- src/ColocMatching/RestBundle/Controller/Rest/v1/AuthenticationController.php
+++ src/ColocMatching/RestBundle/Controller/Rest/v1/AuthenticationController.php
@@ -8,10 +8,14 @@ use ColocMatching\CoreBundle\Exception\InvalidFormException;
 use ColocMatching\CoreBundle\Form\Type\Security\LoginForm;
 use ColocMatching\CoreBundle\Security\User\TokenEncoderInterface;
 use ColocMatching\RestBundle\Exception\AuthenticationException;
+use ColocMatching\RestBundle\Security\OAuth\OAuthConnect;
+use ColocMatching\RestBundle\Security\OAuth\OAuthConnectRegistry;
 use ColocMatching\RestBundle\Security\UserAuthenticationHandler;
+use Doctrine\ORM\ORMException;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\Serializer\SerializerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Operation;
 use Psr\Log\LoggerInterface;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -31,17 +35,21 @@ class AuthenticationController extends AbstractRestController
     /** @var UserAuthenticationHandler */
     private $authenticationHandler;
 
+    /** @var OAuthConnectRegistry */
+    private $oauthConnectRegistry;
+
     /** @var TokenEncoderInterface */
     private $tokenEncoder;
 
 
     public function __construct(LoggerInterface $logger, SerializerInterface $serializer,
         AuthorizationCheckerInterface $authorizationChecker, UserAuthenticationHandler $authenticationHandler,
-        TokenEncoderInterface $tokenEncoder)
+        OAuthConnectRegistry $oauthConnectRegistry, TokenEncoderInterface $tokenEncoder)
     {
         parent::__construct($logger, $serializer, $authorizationChecker);
 
         $this->authenticationHandler = $authenticationHandler;
+        $this->oauthConnectRegistry = $oauthConnectRegistry;
         $this->tokenEncoder = $tokenEncoder;
     }
 
@@ -50,8 +58,7 @@ class AuthenticationController extends AbstractRestController
      * Authenticates a user
      *
      * @Rest\Post(name="rest_authenticate_user")
-     *
-     * @SWG\Post(tags={ "Authentication" },
+     * @Operation(tags={ "Authentication" },
      *   @SWG\Parameter(name="credentials", in="body", @Model(type=LoginForm::class), required=true),
      *   @SWG\Response(
      *     response=201, description="User authenticated",
@@ -89,19 +96,88 @@ class AuthenticationController extends AbstractRestController
 
             $this->logger->info("User authenticated", array ("user" => $user));
 
-            return new JsonResponse(
-                array (
-                    "token" => $token,
-                    "user" => array (
-                        "id" => $user->getId(),
-                        "username" => $user->getUsername(),
-                        "name" => $user->getDisplayName(),
-                        "type" => $user->getType())), Response::HTTP_CREATED);
+            return $this->buildResponse($user, $token);
         }
         catch (InvalidCredentialsException $e)
         {
             throw new AuthenticationException();
         }
+    }
+
+
+    /**
+     * Authenticates a user from an external identity provider
+     *
+     * @Rest\Post(path="/{provider}", name="rest_authenticate_oauth_user", requirements={ "provider"="\w+" })
+     * @Operation(tags={ "Authentication" },
+     *   @SWG\Parameter(
+     *     in="path", name="provider", type="string", required=true, description="The external identity provider name"),
+     *   @SWG\Parameter(name="credentials", in="body", required=true,
+     *     @SWG\Schema(required={"accessToken"},
+     *       @SWG\Property(property="accessToken", type="string", description="Provider access token") )),
+     *   @SWG\Response(
+     *     response=201, description="User authenticated",
+     *     @SWG\Schema(type="object",
+     *       @SWG\Property(property="token", type="string", description="The authentication token"),
+     *       @SWG\Property(property="user", type="object", description="User information",
+     *         @SWG\Property(property="id", type="integer", description="User's identifier", example="1"),
+     *         @SWG\Property(property="username", type="string", description="User's username", example="username"),
+     *         @SWG\Property(property="name", type="string", description="User's display name", example="User User"),
+     *         @SWG\Property(property="type", type="string", description="User's type", example="search") ))
+     *   ),
+     *   @SWG\Response(response=401, description="Authentication error"),
+     *   @SWG\Response(response=403, description="User already authenticated")
+     * )
+     *
+     * @param string $provider
+     * @param Request $request
+     *
+     * @return JsonResponse
+     * @throws ORMException
+     */
+    public function authenticateOAuthUserAction(string $provider, Request $request)
+    {
+        $accessToken = $request->request->get("accessToken");
+
+        $this->logger->debug("Requesting an authentication token for an external provider user",
+            array ("provider" => $provider));
+
+        try
+        {
+            /** @var OAuthConnect $oauthConnect */
+            $oauthConnect = $this->oauthConnectRegistry->get($provider);
+            /** @var UserDto $user */
+            $user = $oauthConnect->handleAccessToken($accessToken);
+            $token = $this->tokenEncoder->encode($user);
+
+            $this->logger->info("User authenticated", array ("user" => $user));
+
+            return $this->buildResponse($user, $token);
+        }
+        catch (InvalidCredentialsException $e)
+        {
+            throw new AuthenticationException("Authentication error on $provider", $e);
+        }
+    }
+
+
+    /**
+     * Build a JsonResponse from the user information and the JWT token
+     *
+     * @param UserDto $user The user
+     * @param string $token The JWT token authenticating the user
+     *
+     * @return JsonResponse
+     */
+    private function buildResponse(UserDto $user, string $token) : JsonResponse
+    {
+        return $this->buildJsonResponse(array (
+            "token" => $token,
+            "user" => array (
+                "id" => $user->getId(),
+                "username" => $user->getUsername(),
+                "name" => $user->getDisplayName(),
+                "type" => $user->getType())), Response::HTTP_CREATED);
     }
 
 }
