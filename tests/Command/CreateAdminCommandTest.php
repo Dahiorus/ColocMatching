10e849d7f5941a3ad757f3068705876c3a7776b4
Dahiorus <brondon.ung@gmail.com>
2018-08-18 14:23:20 +0200

    Correction des tests de Command SF

--- tests/Command/CreateAdminCommandTest.php
+++ tests/Command/CreateAdminCommandTest.php
@@ -7,7 +7,7 @@ use App\Core\Entity\User\UserConstants;
 use App\Core\Exception\EntityNotFoundException;
 use App\Core\Manager\User\UserDtoManagerInterface;
 use App\Tests\Core\AbstractServiceTest;
-use Symfony\Component\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
