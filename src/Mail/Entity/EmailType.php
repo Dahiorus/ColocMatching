5f666739feb3e294b32b20c10fc22a2e8b6420ec
Dahiorus <brondon.ung@gmail.com>
2018-09-06 01:06:36 +0200

    Création des objets pour l'envoi d'e-mail

---
+++ src/Mail/Entity/EmailType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Mail\Entity;
+
+/**
+ * Represents an e-mail content type
+ * @author Dahiorus
+ */
+class EmailType
+{
+    const HTML = "text/html";
+
+    const PLAIN = "text/plain";
+
+}
