7a6063986d9f27ed56bd84464c296cdd2ef376a1
Dahiorus <brondon.ung@gmail.com>
2018-11-15 11:29:00 +0100

    Correction du script de déploiement

--- doctrine-deploy.sh
+++ doctrine-deploy.sh
@@ -6,9 +6,9 @@ then
     php bin/console doctrine:schema:drop --force
 fi
 
-# create the admin user
-php bin/console app:create-admin admin@coloc-matching.fr Secret1234
-
-# create and populate the app schema
+# create the app schema and populate the app schema
 php bin/console doctrine:schema:create
 php bin/console doctrine:fixtures:load -n
+
+# create the admin user
+php bin/console app:create-admin admin@coloc-matching.fr Secret1234
