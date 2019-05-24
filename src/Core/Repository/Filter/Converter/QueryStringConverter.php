c52c1f54b11f812908b6f11d73c878bef8051bd1
Dahiorus <brondon.ung@gmail.com>
2019-05-25 00:15:16 +0200

    Remplacement de Base64StringConverter par un converter de query string

---
+++ src/Core/Repository/Filter/Converter/QueryStringConverter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Core\Repository\Filter\Converter;
+
+use App\Core\Exception\UnsupportedSerializationException;
+use App\Core\Repository\Filter\Searchable;
+use JMS\Serializer\ArrayTransformerInterface;
+use JMS\Serializer\SerializationContext;
+use RuntimeException;
+
+class QueryStringConverter implements StringConverterInterface
+{
+    const PARAM_SEPARATOR = ",";
+
+    /** @var ArrayTransformerInterface */
+    private $arrayTransformer;
+
+
+    public function __construct(ArrayTransformerInterface $arrayTransformer)
+    {
+        $this->arrayTransformer = $arrayTransformer;
+    }
+
+
+    public function toString(Searchable $searchable) : string
+    {
+        $context = SerializationContext::create()->setSerializeNull(false);
+        $array = $this->arrayTransformer->toArray($searchable, $context);
+        $array = array_filter($array, function ($value) {
+            return !empty($value);
+        });
+
+        $queryString = http_build_query($array, null, self::PARAM_SEPARATOR);
+        $queryString = str_replace("=", ":", trim($queryString));
+
+        return urldecode($queryString);
+    }
+
+
+    public function toObject(string $value, string $class) : Searchable
+    {
+        if (!is_subclass_of($class, Searchable::class))
+        {
+            throw new UnsupportedSerializationException($value);
+        }
+
+        $string = str_replace(":", "=", $value);
+        $string = str_replace(self::PARAM_SEPARATOR, "&", $string);
+
+        parse_str($string, $array);
+
+        try
+        {
+            return $this->arrayTransformer->fromArray($array, $class);
+        }
+        catch (RuntimeException $e)
+        {
+            throw new UnsupportedSerializationException($value, $e);
+        }
+    }
+
+}
